6e74f5e6657a76b0864d4a08c90c6037e54e4f3a
Roberto Perez <robertom.perez@outlook.com>
2024-09-03 00:36:54 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+// var numero1 = 50
+// var numero2 = 160
+
+
+// var resultado = numero1 + numero2
+// console.log(resultado)
+// alert(resultado)
+
+
+//  numero1 = prompt('Ingrese el primer numero') 
+ 
+//  numero2 = prompt('Ingrese el segundo numero') 
+
+//  var resultado = Number(numero1) + Number(numero2)
+
+//  alert('El resultado es ' +  resultado)
+
+//  nombre = prompt('Ingrese su nombre')
+//  alert('Hola ' + nombre)
+
+
+// COMENZAMOS CON EL 
+
+
+// Selecto por etiqueta
+// elemento = document.querySelector('p')
+
+// // Selector de clase 
+// menu = document.querySelector('.menu')
+
+// //Selector por ID
+// heroSection = document.querySelector('#Hero-Section')
+
+
+// titulo = document.querySelector('h1')
+// // titulo.innerHTML = 'Este codigo viene de JavaScript'
+// titulo.innerHTML  = "Solo es texto"
+
+
+// Ejersicio1
+
+//  var direccion = document.querySelector('#direccion')
+// direccion.innerHTML = prompt('Ingrese la direccion')
+
+//  var depto = document.querySelector('#depto')
+// depto.innerHTML = prompt('Ingrese el departamento')
+// //Aplicar Estilos (tamaño de fuente)
+// depto.style.fontSize ="24px"
+
+//  var comuna = document.querySelector('#comuna')
+// comuna.innerHTML = prompt('Ingrese la comuna correspondiente')
+
+// Ejersicio2 
+
+var imagen = document.querySelector('img')
+var imagenModificada = prompt("Introduce el valor de border-radius")
+imagen.style.borderRadius = imagenModificada
